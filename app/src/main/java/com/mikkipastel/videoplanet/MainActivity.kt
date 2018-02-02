be2e66628938686e86901d8f2e37792a2c7426b8
Monthira Chayabanjonglerd <monthira@fungjai.com>
2018-02-02 16:41:33 +0700

    Initial commit

---
+++ app/src/main/java/com/mikkipastel/videoplanet/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mikkipastel.videoplanet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.contentContainer, PlayerFragment.newInstance())
+                    .commit()
+        }
+    }
+}
