5f9b8ef7ff151aa4de90a3956d1d2b0c336b9c71
Monthira Chayabanjonglerd <monthira@fungjai.com>
2018-02-12 14:25:52 +0700

    change PlaybackStatus to Kotlin

---
+++ app/src/main/java/com/mikkipastel/videoplanet/player/PlaybackStatus.kt
@@ -0,0 +1,10 @@
+package com.mikkipastel.videoplanet.player
+
+object PlaybackStatus {
+    const val IDLE = "PlaybackStatus_IDLE"
+    const val LOADING = "PlaybackStatus_LOADING"
+    const val PLAYING = "PlaybackStatus_PLAYING"
+    const val PAUSED = "PlaybackStatus_PAUSED"
+    const val STOPPED = "PlaybackStatus_STOPPED"
+    const val ERROR = "PlaybackStatus_ERROR"
+}
